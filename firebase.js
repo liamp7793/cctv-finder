423f79f28d009d7dce1a8005777c1de25262ae78
liamp7793 <107148048+liamp7793@users.noreply.github.com>
2025-03-07 23:57:35 +0000

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBnlctkOpVp0g71o0MO9peptYbtPBJzepQ",
+  authDomain: "cctv-finder-users.firebaseapp.com",
+  projectId: "cctv-finder-users",
+  storageBucket: "cctv-finder-users.firebasestorage.app",
+  messagingSenderId: "435511062817",
+  appId: "1:435511062817:web:1c0d8a9eb69d794850ce60"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
