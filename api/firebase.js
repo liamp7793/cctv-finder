e2c7aa9c3506a29f57d08587e27acd2f554e0a34
liamp7793 <107148048+liamp7793@users.noreply.github.com>
2025-03-08 00:30:15 +0000

    Create firebase.js

---
+++ api/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBnlctkOpVp0g71o0MO9peptYbtPBJzepQ",
+  authDomain: "cctv-finder-users.firebaseapp.com",
+  projectId: "cctv-finder-users",
+  storageBucket: "cctv-finder-users.firebasestorage.app",
+  messagingSenderId: "435511062817",
+  appId: "1:435511062817:web:1c0d8a9eb69d794850ce60"
+};
